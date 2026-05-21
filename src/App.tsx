dfc4e4d7c69b5bce24a886ad2acc0827670e7fb3
Guilherme Amaral Dourado <guilhermeamaraldourado@gmail.com>
2026-05-21 19:14:26 -0300

    function + css

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   console.log('Oi');
 
   return (
     <>
-      <Heading />
-      <Heading />
+      <Heading>
+         Olá mundo! 
+         <button>
+          <TimerIcon />
+         </button>
+      </Heading>
+      <Heading>Olá mundo 2!</Heading>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
         incidunt accusamus commodi quasi sit, doloribus in fugiat, repellat
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,18 @@
+import React from 'react';
 import styles from './Heading.module.css';
 
-export function Heading() {
-  return <h1 className={styles.heading}>Olá mundo!</h1>;
+// type HeadingProps = {
+//   children: string;
+// };
+
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+//export function Heading(props: HeadingProps) {
+export function Heading({ children }: HeadingProps) {
+  console.log(children);
+  
+  //return <h1 className={styles.heading}>{props.children}</h1>;
+  return <h1 className={styles.heading}>{children}</h1>;
 }
