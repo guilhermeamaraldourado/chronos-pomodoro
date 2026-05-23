f36ca7c26cfdf5352019b659426ca14da1c8e4b7
Guilherme Amaral Dourado <guilhermeamaraldourado@gmail.com>
2026-05-23 10:00:06 -0300

    Form + DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+import { Countdown } from "./components/Countdown";
 
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Menu } from "./components/Menu";
+import { DefaultInput } from "./components/DefaultInput";
 
 export function App() {
   return (
@@ -11,8 +13,39 @@ export function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <Countdown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput
+              labelText="task"
+              id="meuInput"
+              type="text"
+              placeholder="Digite algo"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
