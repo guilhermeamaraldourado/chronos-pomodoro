e5d258fe5cd72a771c9234d90937fe22c461f0e3
Guilherme Amaral Dourado <guilhermeamaraldourado@gmail.com>
2026-05-26 11:58:07 -0300

    Generic HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,8 @@
-import { Container } from "./components/Container";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import { Countdown } from "./components/Countdown";
-import { DefaultInput } from "./components/DefaultInput";
-import { Cycles } from "./components/Cycles";
-import { DefaultButton } from "./components/DefaultButton";
-import { PlayCircleIcon } from "lucide-react";
-import { Footer } from "./components/Footer";
-
 import "./styles/theme.css";
 import "./styles/global.css";
 
+import { Home } from "./pages/Home";
+
 export function App() {
-  return (
-    <ThemeProvider>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <Countdown />
-      </Container>
-
-      <Container>
-        <form className="form" action="">
-          <div className="formRow">
-            <DefaultInput
-              labelText="task 1"
-              id="meuInput"
-              type="text"
-              placeholder="Digite algo"
-            />
-          </div>
-
-          <div className="formRow">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className="formRow">
-            <Cycles />
-          </div>
-
-          <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </ThemeProvider>
-  );
+  return <Home />;
 }
